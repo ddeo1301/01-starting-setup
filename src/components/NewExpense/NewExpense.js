9b092ade979523ebf2926709ff45fe8d894d5bf9
ddeo1301 <divyanshudeo1301@gmail.com>
2023-08-31 18:53:19 +0530

    DIY - Challenges on Conditional Rendering

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import {useState} from 'react';
 import ExpenseForm from './ExpenseForm';
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+    let defaultForm = (
+        <div className="new-expense__controls">
+          <div className="new-expense__actions">
+            <button onClick={showFormHandler}>Add Expense</button>
+          </div>
+        </div>
+    );
 
-    const saveExpenseDataHandler = (enteredExpenseData) => {
+    const [entereddefaultForm, setDefaultForm] = useState(defaultForm);
+
+    function saveExpenseDataHandler(enteredExpenseData){
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
@@ -13,10 +22,18 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData);
     }
 
-    return <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
-        {/* didnt add () so that fn itself is added to expenseForm */}
-    </div>
+    function showFormHandler() {
+        setDefaultForm(
+            <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler}></ExpenseForm>
+        );
+    }
+
+    return <div className="new-expense">{entereddefaultForm}</div>;
+
+    // return <div className='new-expense'>
+    //     <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
+    //     {/* didnt add () so that fn itself is added to expenseForm */}
+    // </div>
 };
 
 export default NewExpense;
